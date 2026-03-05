d68dd1a6678e0c4bfe7b2ad75d17bbb35c6f5b21
Rachana Angara <rangara1@asu.edu>
2026-03-04 23:52:30 -0700

    landing page code

--- frontend/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://dofckgebpdpglbyqlwei.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRvZmNrZ2VicGRwZ2xieXFsd2VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2NzIwMzQsImV4cCI6MjA4ODI0ODAzNH0.PtdoIz2S2gFb77xnlLnfRxuvZmyMn08OQMVGTtMkjBk'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
